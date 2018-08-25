89ca18014979e6e31793193f8540f474e6da2ed8
Paul Zietsman <phzietsman@gmail.com>
2018-08-25 21:21:08 +0200

    Ignore truffle-secret.js

--- truffle-secrets.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mnemonic: "taxi relief vocal teach glimpse chaos hedgehog blast sting donor almost term",
-  infura: "https://rinkeby.infura.io/v3/35b9e955611b49289ef913fd9b82aada"
-}
